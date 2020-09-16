391b5be08b1001187b8b6591a2e9d6e4aaf448e2
tharuPrerera <tharuDaniyal@gmail.com>
2020-09-16 17:52:57 +0530

    Updete UI

---
+++ NIBM_COVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBM_COVID19
+//
+//  Created by Digital-03 on 9/14/20.
+//  Copyright © 2020 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
